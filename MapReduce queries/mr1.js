6abaa24a67586bc4b00e939712e3d636148f5d52
Alen Murtić <Murta-thelegend@hotmail.com>
2017-01-13 00:01:02 +0100

    Promijenio mr1 da vraća više u duhu zadatka

--- MapReduce queries/mr1.js
+++ MapReduce queries/mr1.js
@@ -5,22 +5,33 @@ var map = function() {
                 count++;
             }
         if(count != 0)
-            emit(this._id, {title: this.title, count : count});
+            emit(0, {id : this._id, title: this.title, count : count});
 };
 
-var reduce = function(key, values) {
-    return values;
+var reduce = function(key, values) {
+    result = {};
+    values = values.sort(function(a,b){
+        return b.count-a.count;
+    });
+    for(var i = 0; i < values.length; i++){
+        result[i] = values[i];
+    }
+    return result;
 };
+
+var fin = function(key, reducedVal){
+    
+}
 
 db.news.mapReduce(
 	map,
 	reduce,
 	{out: "mr1_news"}
 );
-//db.mr1_news.find().sort({value : -1});
-
-db.mr1_news.aggregate(
-   [
-     { $sort: { "value.count": -1 } }
-   ]
+//db.mr1_news.find().sort({value : -1});
+
+db.mr1_news.aggregate(
+   [
+     { $sort: { "value.count": -1 } }
+   ]
 )
---
+++ MapReduce queries/mr1_old.js
@@ -0,0 +1,26 @@
+var map = function() {
+    var count = 0;
+	if (this.comments !== undefined)
+            for(com in this.comments){
+                count++;
+            }
+        if(count != 0)
+            emit(this._id, {title: this.title, count : count});
+};
+
+var reduce = function(key, values) {
+    return values;
+};
+
+db.news.mapReduce(
+	map,
+	reduce,
+	{out: "mr1_news"}
+);
+//db.mr1_news.find().sort({value : -1});
+
+db.mr1_news.aggregate(
+   [
+     { $sort: { "value.count": -1 } }
+   ]
+)
